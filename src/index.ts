657b46f86eaa11aa07403a32f8898589c1cf491d
Dean Rikrik Ichsan Hakiki <deanrih@users.noreply.github.com>
2026-04-23 10:16:17 +0700

    fix?: struggling with the build output
    Signed-off-by: Dean Rikrik Ichsan Hakiki <deanrih@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -15,54 +15,65 @@
 /** biome-ignore-all assist/source/organizeImports: aesthetic */
 /** biome-ignore-all lint/performance/noBarrelFile: library */
 
-export { forma } from "~/impl/forma";
-export { Problem } from "~/impl/problem";
-export { Success } from "~/impl/success";
+// export { forma } from "~/impl/forma";
+// export { Problem } from "~/impl/problem";
+// export { Success } from "~/impl/success";
 
-export {
-	BadGateway,
-	BadRequest,
-	Conflict,
-	ContentTooLarge,
-	ExpectationFailed,
-	FailedDependency,
-	Forbidden,
-	GatewayTimeout,
-	Gone,
-	HttpVersionNotSupported,
-	ImATeapot,
-	InsufficientStorage,
-	InternalServerError,
-	LengthRequired,
-	Locked,
-	LoopDetected,
-	MethodNotAllowed,
-	MisdirectedRequest,
-	NetworkAuthenticationRequired,
-	NotAcceptable,
-	NotExtended,
-	NotFound,
-	NotImplemented,
-	PaymentRequired,
-	PreconditionFailed,
-	PreconditionRequired,
-	ProxyAuthenticationRequired,
-	RangeNotSatisfiable,
-	RequestHeaderFieldsTooLarge,
-	RequestTimeout,
-	ServiceUnavailable,
-	TooEarly,
-	TooManyRequests,
-	Unauthorized,
-	UnavailableForLegalReasons,
-	UnprocessableContent,
-	UnsupportedMediaType,
-	UpgradeRequired,
-	UriTooLong,
-	VariantAlsoNegotiates,
-} from "~/impl/problem-list";
-export { Ok } from "~/impl/success-list";
+// export {
+// 	BadGateway,
+// 	BadRequest,
+// 	Conflict,
+// 	ContentTooLarge,
+// 	ExpectationFailed,
+// 	FailedDependency,
+// 	Forbidden,
+// 	GatewayTimeout,
+// 	Gone,
+// 	HttpVersionNotSupported,
+// 	ImATeapot,
+// 	InsufficientStorage,
+// 	InternalServerError,
+// 	LengthRequired,
+// 	Locked,
+// 	LoopDetected,
+// 	MethodNotAllowed,
+// 	MisdirectedRequest,
+// 	NetworkAuthenticationRequired,
+// 	NotAcceptable,
+// 	NotExtended,
+// 	NotFound,
+// 	NotImplemented,
+// 	PaymentRequired,
+// 	PreconditionFailed,
+// 	PreconditionRequired,
+// 	ProxyAuthenticationRequired,
+// 	RangeNotSatisfiable,
+// 	RequestHeaderFieldsTooLarge,
+// 	RequestTimeout,
+// 	ServiceUnavailable,
+// 	TooEarly,
+// 	TooManyRequests,
+// 	Unauthorized,
+// 	UnavailableForLegalReasons,
+// 	UnprocessableContent,
+// 	UnsupportedMediaType,
+// 	UpgradeRequired,
+// 	UriTooLong,
+// 	VariantAlsoNegotiates,
+// } from "~/impl/problem-list";
+// export { Ok } from "~/impl/success-list";
 
-export { httpStatus, httpStatusCode, httpStatusName, isStatusError } from "~/impl/util";
+// export { httpStatus, httpStatusCode, httpStatusName, isStatusError } from "~/impl/util";
 
-export type { HttpStatus, HttpStatusCode, HttpStatusName } from "~/impl/type";
+// export type { HttpStatus, HttpStatusCode, HttpStatusName } from "~/impl/type";
+
+export * from "~/impl/forma";
+export * from "~/impl/problem";
+export * from "~/impl/success";
+
+export * from "~/impl/problem-list";
+export * from "~/impl/success-list";
+
+export * from "~/impl/util";
+
+export type * from "~/impl/type";
